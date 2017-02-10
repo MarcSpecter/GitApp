5078b0eb94ad6860f3b1589b6f4fe540e4c294f1
Marc Specter <marcspecter@Apples-MacBook-Pro.local>
2017-02-10 01:04:43 +0000

    Commit

---
+++ GitApp/UsefulFunctions.hpp
@@ -0,0 +1,17 @@
+//
+//  UsefulFunctions.hpp
+//  GitApp
+//
+//  Created by Marc Specter on 09/02/2017.
+//  Copyright © 2017 Marc Specter. All rights reserved.
+//
+
+#ifndef UsefulFunctions_hpp
+#define UsefulFunctions_hpp
+
+#include <stdio.h>
+
+void convert(const char* in, wchar_t* out);
+
+
+#endif /* UsefulFunctions_hpp */
